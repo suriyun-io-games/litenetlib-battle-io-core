90419fd42e5326a96c37081987595bf59dfce466
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-09-26 14:17:34 +0700

    Add new UIs

--- UI/UIMainMenu.cs
+++ UI/UIMainMenu.cs
@@ -5,6 +5,12 @@ using UnityEngine.UI;
 
 public class UIMainMenu : MonoBehaviour
 {
+    public enum PreviewState
+    {
+        Idle,
+        Run,
+        Dead,
+    }
     public Text textSelectCharacter;
     public Text textSelectHead;
     public Text textSelectWeapon;
@@ -27,10 +33,11 @@ public class UIMainMenu : MonoBehaviour
     private int selectHead = 0;
     private int selectWeapon = 0;
     // Showing character / items
-    private CharacterModel characterModel;
-    private CharacterData characterData;
-    private HeadData headData;
-    private WeaponData weaponData;
+    public CharacterModel characterModel;
+    public CharacterData characterData;
+    public HeadData headData;
+    public WeaponData weaponData;
+    public PreviewState previewState;
 
     public int SelectCharacter
     {
@@ -91,10 +98,7 @@ public class UIMainMenu : MonoBehaviour
 
     private void Start()
     {
-        inputName.text = PlayerSave.GetPlayerName();
-        SelectHead = PlayerSave.GetHead();
-        SelectCharacter = PlayerSave.GetCharacter();
-        SelectWeapon = PlayerSave.GetWeapon();
+        OnClickLoadData();
     }
 
     private void Update()
@@ -126,6 +130,41 @@ public class UIMainMenu : MonoBehaviour
             textDamageRateLeechHp.text = (totalStats.addDamageRateLeechHp * 100).ToString("N0") + "%";
         if (textSpreadDamages != null)
             textSpreadDamages.text = (totalStats.addSpreadDamages * 100).ToString("N0") + "%";
+
+        if (characterModel != null)
+        {
+            var animator = characterModel.TempAnimator;
+            switch (previewState)
+            {
+                case PreviewState.Idle:
+                    animator.SetBool("IsDead", false);
+                    animator.SetFloat("JumpSpeed", 0);
+                    animator.SetFloat("MoveSpeed", 0);
+                    animator.SetBool("IsGround", true);
+                    animator.SetBool("IsDash", false);
+                    animator.SetBool("DoAction", false);
+                    animator.SetBool("IsIdle", true);
+                    break;
+                case PreviewState.Run:
+                    animator.SetBool("IsDead", false);
+                    animator.SetFloat("JumpSpeed", 0);
+                    animator.SetFloat("MoveSpeed", 1);
+                    animator.SetBool("IsGround", true);
+                    animator.SetBool("IsDash", false);
+                    animator.SetBool("DoAction", false);
+                    animator.SetBool("IsIdle", false);
+                    break;
+                case PreviewState.Dead:
+                    animator.SetBool("IsDead", true);
+                    animator.SetFloat("JumpSpeed", 0);
+                    animator.SetFloat("MoveSpeed", 0);
+                    animator.SetBool("IsGround", true);
+                    animator.SetBool("IsDash", false);
+                    animator.SetBool("DoAction", false);
+                    animator.SetBool("IsIdle", false);
+                    break;
+            }
+        }
     }
 
     private void UpdateCharacter()
@@ -144,6 +183,8 @@ public class UIMainMenu : MonoBehaviour
         if (weaponData != null)
             characterModel.SetWeaponModel(weaponData.rightHandObject, weaponData.leftHandObject, weaponData.shieldObject);
         characterModel.gameObject.SetActive(true);
+        var animator = characterModel.TempAnimator;
+        animator.SetInteger("WeaponAnimId", weaponData != null ? weaponData.weaponAnimId : 0);
     }
 
     private void UpdateHead()
@@ -157,7 +198,11 @@ public class UIMainMenu : MonoBehaviour
     {
         weaponData = GameInstance.GetAvailableWeapon(SelectWeapon);
         if (characterModel != null && weaponData != null)
+        {
             characterModel.SetWeaponModel(weaponData.rightHandObject, weaponData.leftHandObject, weaponData.shieldObject);
+            var animator = characterModel.TempAnimator;
+            animator.SetInteger("WeaponAnimId", weaponData.weaponAnimId);
+        }
     }
 
     public void OnClickBackCharacter()
@@ -195,22 +240,24 @@ public class UIMainMenu : MonoBehaviour
         PlayerSave.SetPlayerName(inputName.text);
     }
 
-    public void OnClickLan()
+    public void OnClickSaveData()
     {
         PlayerSave.SetCharacter(SelectCharacter);
         PlayerSave.SetHead(SelectHead);
         PlayerSave.SetWeapon(SelectWeapon);
         PlayerSave.SetPlayerName(inputName.text);
+    }
+
+    public void OnClickLan()
+    {
+        OnClickSaveData();
         if (lanNetworkingDialog != null)
             lanNetworkingDialog.Show();
     }
 
     public void OnClickOnline()
     {
-        PlayerSave.SetCharacter(SelectCharacter);
-        PlayerSave.SetHead(SelectHead);
-        PlayerSave.SetWeapon(SelectWeapon);
-        PlayerSave.SetPlayerName(inputName.text);
+        OnClickSaveData();
         if (!string.IsNullOrEmpty(onlineNetworkAddress) && onlineNetworkPort >= 0)
         {
             var networkManager = GameNetworkManager.Singleton;
@@ -221,6 +268,14 @@ public class UIMainMenu : MonoBehaviour
         }
         if (enterNetworkAddressDialog != null)
             enterNetworkAddressDialog.Show();
+    }
+
+    public void OnClickLoadData()
+    {
+        inputName.text = PlayerSave.GetPlayerName();
+        SelectHead = PlayerSave.GetHead();
+        SelectCharacter = PlayerSave.GetCharacter();
+        SelectWeapon = PlayerSave.GetWeapon();
     }
 
     public void UpdateAvailableItems()
