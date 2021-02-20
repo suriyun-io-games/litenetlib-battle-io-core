05b087097e4a74ef8d37b76a9dea950c2c2458a8
Insthync <ittipon.bay@gmail.com>
2021-02-21 00:46:32 +0700

    Change how to interpolate position

--- GamePlay/CharacterEntity.cs
+++ GamePlay/CharacterEntity.cs
@@ -371,7 +371,7 @@ public class CharacterEntity : BaseNetworkGameCharacter
         if (NetworkManager != null && NetworkManager.IsMatchEnded)
             return;
 
-        CacheNetTransform.interpolateMode = LiteNetLibTransform.InterpolateMode.EstimatedSpeed;
+        CacheNetTransform.interpolateMode = LiteNetLibTransform.InterpolateMode.FixedSpeed;
         CacheNetTransform.fixedInterpolateSpeed = TotalMoveSpeed;
 
         if (Hp <= 0)
